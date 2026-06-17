43010876726969aeb8db41be8e37d140fb9a12de
anufrievalex <gjsasha613@gmail.com>
2026-06-17 17:47:20 +0700

    Теперь в обращениях можно менять контакты в случае ошибки

--- src/widgets/admin/appeals/ClientQuestionCard.tsx
+++ src/widgets/admin/appeals/ClientQuestionCard.tsx
@@ -1,12 +1,17 @@
 import { AppealsRepo } from "@/data/repos/AppealsRepo";
 import { AppealStatus, ClientQuestionDto } from "@/domain"
+import { UpdateContactsFormValues, updateContactsSchema } from "@/domain/schemas/admin/appeals/appeal";
+import { useUpdateAppealContacts } from "@/features/appeals/useUpdateAppealContacts";
 import { useUpdateAppealStatus } from "@/features/appeals/useUpdateAppealStatus";
 import styles from "@/shared/styles/admin/appeals/client-question.module.scss";
 import { getCategoryLabel, getStatusColor, getStatusLabel } from "@/shared/utils/appealHelpers";
-import { Badge, Group, Select, Stack, Text, Title } from "@mantine/core";
-import { IconChevronDown } from "@tabler/icons-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ActionIcon, Badge, Button, Group, InputBase, Modal, Select, Stack, Text, TextInput, Title } from "@mantine/core";
+import { IconChevronDown, IconPencil } from "@tabler/icons-react";
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { IMaskInput } from "react-imask";
 
 interface CardProps {
     question: ClientQuestionDto;
@@ -16,7 +21,33 @@ const repo = new AppealsRepo();
 
 export const ClientQuestionCard = ({question}: CardProps) => {
     const updateStatus = useUpdateAppealStatus(repo);
+    const updateContacts = useUpdateAppealContacts(repo);
     const [status, setStatus] = useState(question.status);
+    const [modalOpened, setModalOpened] = useState(false);
+
+    const form = useForm<UpdateContactsFormValues>({
+        defaultValues: {
+            contactEmail: undefined,
+            contactPhone: undefined
+        },
+        resolver: zodResolver(updateContactsSchema)
+    });
+    const onSaveContacts = (data: UpdateContactsFormValues) => {
+        updateContacts.mutate({
+            id: question.id,
+            req: data
+        });
+        setModalOpened(false);
+        form.reset();
+    }
+    useEffect(() => {
+        if (question) {
+            form.reset({
+                contactEmail: question.contactEmail,
+                contactPhone: question.contactPhone
+            });
+        }
+    }, [form, question]);
     return (
         <Stack classNames={{root: styles.card}}>
             <Group justify="space-between">
@@ -45,10 +76,30 @@ export const ClientQuestionCard = ({question}: CardProps) => {
             <Stack gap={8}>
                 <Text classNames={{root: styles.info}}>Создано: <span>{dayjs(question.createdAt).format("DD.MM.YYYY в HH:mm")}</span></Text>
                 <Text classNames={{root: styles.info}}>Категория: <span>{getCategoryLabel(question.category)}</span></Text>
-                <Text classNames={{root: styles.info}}>Телефон: <span>{question.contactPhone}</span></Text>
+                <Group justify="space-between">
+                    <Text classNames={{root: styles.info}}>Телефон: <span>{question.contactPhone}</span></Text>
+                    <Button onClick={() => setModalOpened(true)} classNames={{
+                        root: styles.submitBtn
+                    }} leftSection={<IconPencil></IconPencil>}>Изменить контакты</Button>
+                </Group>
                 <Text classNames={{root: styles.info}}>Email: <span>{question.contactEmail}</span></Text>
                 <Text classNames={{root: styles.info}}>Комментарий менеджера: <span>{question.managerComment || "Нет"}</span></Text>
             </Stack>
+            <Modal title="Изменение контактов" opened={modalOpened} onClose={() => setModalOpened(false)}>
+                <form onSubmit={form.handleSubmit(onSaveContacts)}>
+                    <Stack gap="md">
+                        <TextInput type="email" label="Email" {...form.register("contactEmail")} error={
+                            form.formState.errors.contactEmail?.message
+                        } classNames={{input: styles.input}}></TextInput>
+                        <Controller control={form.control} name="contactPhone" render={({field}) => (
+                            <InputBase component={IMaskInput} mask="+7 (000) 000-00-00" {...field} error={
+                                form.formState.errors.contactPhone?.message
+                            } classNames={{input: styles.input}} label="Номер телефона"></InputBase>
+                        )}></Controller>
+                        <Button type="submit" classNames={{root: styles.submitBtn}}>Обновить контакты</Button>
+                    </Stack>
+                </form>
+            </Modal>
         </Stack>
     )
 }
--- src/widgets/admin/appeals/SupplierRequestCard.tsx
+++ src/widgets/admin/appeals/SupplierRequestCard.tsx
@@ -1,12 +1,17 @@
 import { AppealsRepo } from "@/data/repos/AppealsRepo";
 import { AppealStatus, SupplierRequestDto } from "@/domain";
+import { UpdateContactsFormValues, updateContactsSchema } from "@/domain/schemas/admin/appeals/appeal";
+import { useUpdateAppealContacts } from "@/features/appeals/useUpdateAppealContacts";
 import { useUpdateAppealStatus } from "@/features/appeals/useUpdateAppealStatus";
 import styles from "@/shared/styles/admin/appeals/supplier-request.module.scss";
 import { getStatusColor, getCategoryLabel } from "@/shared/utils/appealHelpers";
-import { Stack, Group, Title, Select, Text } from "@mantine/core";
-import { IconChevronDown } from "@tabler/icons-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Stack, Group, Title, Select, Text, Button, InputBase, Modal, TextInput } from "@mantine/core";
+import { IconChevronDown, IconPencil } from "@tabler/icons-react";
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { IMaskInput } from "react-imask";
 
 interface CardProps {
     request: SupplierRequestDto;
@@ -16,7 +21,33 @@ const repo = new AppealsRepo();
 
 export const SupplierRequestCard = ({request}: CardProps) => {
     const updateStatus = useUpdateAppealStatus(repo);
+    const updateContacts = useUpdateAppealContacts(repo);
     const [status, setStatus] = useState(request.status);
+    const [modalOpened, setModalOpened] = useState(false);
+
+    const form = useForm<UpdateContactsFormValues>({
+        defaultValues: {
+            contactEmail: undefined,
+            contactPhone: undefined
+        },
+        resolver: zodResolver(updateContactsSchema)
+    });
+    const onSaveContacts = (data: UpdateContactsFormValues) => {
+        updateContacts.mutate({
+            id: request.id,
+            req: data
+        });
+        setModalOpened(false);
+        form.reset();
+    }
+    useEffect(() => {
+        if (request) {
+            form.reset({
+                contactEmail: request.contactEmail,
+                contactPhone: request.contactPhone
+            });
+        }
+    }, [form, request]);
     return (
         <Stack classNames={{root: styles.card}}>
             <Group justify="space-between">
@@ -43,10 +74,30 @@ export const SupplierRequestCard = ({request}: CardProps) => {
             <Stack gap={8}>
                 <Text classNames={{root: styles.info}}>Создано: <span>{dayjs(request.createdAt).format("DD.MM.YYYY в HH:mm")}</span></Text>
                 <Text classNames={{root: styles.info}}>Компания: <span>{request.companyName}</span></Text>
-                <Text classNames={{root: styles.info}}>Телефон: <span>{request.contactPhone}</span></Text>
+                <Group justify="space-between">
+                    <Text classNames={{root: styles.info}}>Телефон: <span>{request.contactPhone}</span></Text>
+                    <Button onClick={() => setModalOpened(true)} classNames={{
+                        root: styles.submitBtn
+                    }} leftSection={<IconPencil></IconPencil>}>Изменить контакты</Button>
+                </Group>
                 <Text classNames={{root: styles.info}}>Email: <span>{request.contactEmail}</span></Text>
                 <Text classNames={{root: styles.info}}>Комментарий менеджера: <span>{request.managerComment || "Нет"}</span></Text>
             </Stack>
+            <Modal title="Изменение контактов" opened={modalOpened} onClose={() => setModalOpened(false)}>
+                <form onSubmit={form.handleSubmit(onSaveContacts)}>
+                    <Stack gap="md">
+                        <TextInput type="email" label="Email" {...form.register("contactEmail")} error={
+                            form.formState.errors.contactEmail?.message
+                        } classNames={{input: styles.input}}></TextInput>
+                        <Controller control={form.control} name="contactPhone" render={({field}) => (
+                            <InputBase component={IMaskInput} mask="+7 (000) 000-00-00" {...field} error={
+                                form.formState.errors.contactPhone?.message
+                            } classNames={{input: styles.input}} label="Номер телефона"></InputBase>
+                        )}></Controller>
+                        <Button type="submit" classNames={{root: styles.submitBtn}}>Обновить контакты</Button>
+                    </Stack>
+                </form>
+            </Modal>
         </Stack>
     )
 }
