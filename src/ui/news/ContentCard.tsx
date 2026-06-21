94ade6f63c341d0b5cfcac6f16e4d30c2f246bba
anufrievalex <gjsasha613@gmail.com>
2026-06-21 11:20:18 +0700

    Сменил палитру и шрифт + поправил мелкие шероховатости

--- src/ui/news/ContentCard.tsx
+++ src/ui/news/ContentCard.tsx
@@ -1,22 +1,18 @@
-import { Group, Stack, Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
 import styles from "@/shared/styles/contentCard/card.module.scss";
 import dayjs from "dayjs";
 
 interface CardProps {
-    type: "News" | "Promo";
     title: string;
     description?: string;
     createdAt: string;
     imagePath?: string;
 }
 
-export const ContentCard = ({type, title, description, createdAt, imagePath}: CardProps) => {
+export const ContentCard = ({title, description, createdAt, imagePath}: CardProps) => {
     return (
         <Stack gap="md" classNames={{root: styles.card}}>
-            <Group justify="space-between" wrap="wrap" gap="xs">
-                <Text c="dimmed" size="sm">Дата публикации: {dayjs(createdAt).format("DD.MM.YYYY в HH:mm")}</Text>
-                <Text classNames={{root: `${styles.cardType} ${styles[type]}`}}>{type === "News" ? "Новость" : "Акция"}</Text>
-            </Group>
+            <Text c="dimmed" size="sm">Дата публикации: {dayjs(createdAt).format("DD.MM.YYYY в HH:mm")}</Text>
             <Text classNames={{root: styles.title}}>{title}</Text>
             {imagePath && (
                 <img src={imagePath} alt={title} className={styles.cardImg}></img>
